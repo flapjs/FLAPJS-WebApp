bc4549a10ebcf0671176c42c25a6473b05e89ced
Andrew <andykuo1supergreen@gmail.com>
2018-07-16 10:11:10 -0700

    Removed hamburger from toolbar

--- src/app/toolbar/Toolbar.js
+++ src/app/toolbar/Toolbar.js
@@ -11,12 +11,6 @@ class Toolbar extends React.Component
     const drawer = document.getElementsByTagName('Drawer')[0];
     return <div className="toolbar-container">
         <h1>Deterministic Finite Automata</h1>
-        <div
-          onClick={app.state.isOpen ?
-            app.closeDrawer.bind(app) :
-            app.openDrawer.bind(app)}>
-          <HamburgerIcon active={app.state.isOpen}/>
-        </div>
     </div>;
   }
 }
