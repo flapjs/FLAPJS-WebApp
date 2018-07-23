45a3468e5ab6dbe8ef53a671d3779ba131078c87
Xuanqiang Zhao <vsastray@outlook.com>
2018-07-23 15:27:37 -0700

    page404

--- src/pages/404/Page404.js
+++ src/pages/404/Page404.js
@@ -9,14 +9,19 @@ class Page404 extends React.Component
     super(props);
   }
 
+  onClick(e)
+  {
+    this.props.router.pathname = "/";
+  }
+
   render()
   {
     return(
         <div className="four04-container">
           <h1 className="four04-heading">Page Not Found</h1>
-          <a href="https://www.google.com">
+          <div className="four04-logo" onClick={this.onClick.bind(this)}>
             <img src={logo404} alt="logo404" height="100px" />
-          </a>
+          </div>
           <p className="four04-text">Sorry, the page you're looking for isn't available.</p>
         </div>
     );
