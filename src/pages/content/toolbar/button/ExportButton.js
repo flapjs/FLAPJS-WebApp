a1a93c9ec18740a859dd09f5bf8c38bd07b4fd8d
Xuanqiang Zhao <vsastray@outlook.com>
2018-07-30 16:17:09 -0700

    rotate export icon

--- src/pages/content/toolbar/button/ExportButton.js
+++ src/pages/content/toolbar/button/ExportButton.js
@@ -21,7 +21,9 @@ class ExportButton extends React.Component
                    onClick={this.onClick.bind(this)}>
       <svg className="navicons" viewBox="0 0 24 24">
         <title>Export</title>
-        <path d="M23,12L19,8V11H10V13H19V16M1,18V6C1,4.89 1.9,4 3,4H15A2,2 0 0,1 17,6V9H15V6H3V18H15V15H17V18A2,2 0 0,1 15,20H3A2,2 0 0,1 1,18Z" />
+        <g transform="rotate(-90, 12, 12)">
+          <path d="M23,12L19,8V11H10V13H19V16M1,18V6C1,4.89 1.9,4 3,4H15A2,2 0 0,1 17,6V9H15V6H3V18H15V15H17V18A2,2 0 0,1 15,20H3A2,2 0 0,1 1,18Z" />
+        </g>
       </svg>
     </button>;
   }
