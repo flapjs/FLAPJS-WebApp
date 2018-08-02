0b8846b54bd753e0cc5694be39c19342ec582617
l1lang <langlixuan1@gmail.com>
2018-08-02 16:52:27 -0700

    Add import xml file function

--- src/graph/GraphUploader.js
+++ src/graph/GraphUploader.js
@@ -27,6 +27,35 @@ class GraphUploader
     };
     reader.readAsText(fileBlob);
   }
+
+  static uploadJFLAPToGraph(fileBlob, graph, callback=null, errorCallback=null)
+  {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const data = event.target.result;
+      try
+      {
+        let parser = new DOMParser();
+        const dataXML = parser.parseFromString(data, "text/xml");
+        const dst = NodalGraph.parseXML(dataXML);
+
+        graph.copyGraph(dst);
+
+        if (callback) callback();
+      }
+      catch(e)
+      {
+        console.log(e);
+        reader.abort();
+
+        if (errorCallback) errorCallback();
+      }
+    };
+    reader.onerror = (event) => {
+      if (errorCallback) errorCallback(event.target.error.code);
+    };
+    reader.readAsText(fileBlob);
+  }
 }
 
 export default GraphUploader;
