c7adb948fa2baebadcc269defb85f04d5acc902a
Andrew <andykuo1supergreen@gmail.com>
2018-08-02 18:01:48 -0700

    Implemented definition framework

---
+++ src/pages/content/drawer/panels/definition/StateList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class StateList extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render()
+  {
+    return <div className="statelist-container">
+    </div>
+  }
+}
+
+export default StateList;
