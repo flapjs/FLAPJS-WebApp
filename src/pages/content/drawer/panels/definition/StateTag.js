2a3b84bdefda381f279a2d135e7a838b3d28f300
Andrew <andykuo1supergreen@gmail.com>
2018-08-02 18:02:35 -0700

    Added StateTag

---
+++ src/pages/content/drawer/panels/definition/StateTag.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class StateTag extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render()
+  {
+    return <div className="statetag-container">
+    </div>
+  }
+}
+
+export default StateTag;
