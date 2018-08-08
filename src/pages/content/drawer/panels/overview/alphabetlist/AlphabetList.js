838b212c021cde4c6a58a7f85db057c2a1130cef
Xuanqiang Zhao <vsastray@outlook.com>
2018-08-08 13:47:10 -0700

    sort alphabet

--- src/pages/content/drawer/panels/overview/alphabetlist/AlphabetList.js
+++ src/pages/content/drawer/panels/overview/alphabetlist/AlphabetList.js
@@ -23,7 +23,7 @@ class AlphabetList extends React.Component
 
   render()
   {
-    const alphabet = this.props.machine.getAlphabet();
+    const alphabet = this.props.machine.getAlphabet().sort();
     return <InfoBlock title="Alphabet">
       <div className="alphalist-container">
         <div className="alphalist">
