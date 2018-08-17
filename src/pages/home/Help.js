8c8b00aaf4114774cdb3f9b0df22ce43461f59db
David Osuna <dosunanet@gmail.com>
2018-08-16 17:11:47 -0700

    Open Help.md in a new tab

--- src/pages/home/Help.js
+++ src/pages/home/Help.js
@@ -7,9 +7,11 @@ class Help extends React.Component{
         super(props);
     }
 
-    onClick(ev)
+    onClick()
     {
-        window.location.href="https://github.com/flapjs/FLAPJS-WebApp/blob/intro_page/src/pages/home/Help.md";
+        //window.location.href="https://github.com/flapjs/FLAPJS-WebApp/blob/intro_page/src/pages/home/Help.md";
+        var win = window.open("https://github.com/flapjs/FLAPJS-WebApp/blob/intro_page/src/pages/home/Help.md", '_blank');
+        win.focus();
     }
 
     render()
