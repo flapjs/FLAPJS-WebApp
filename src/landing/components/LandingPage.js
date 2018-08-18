4d0d6eaa2ee237031a1e42348d7e8e483a3f3ccb
Andrew <andykuo1supergreen@gmail.com>
2018-08-17 19:11:56 -0700

    Properly uppercase github

--- src/landing/components/LandingPage.js
+++ src/landing/components/LandingPage.js
@@ -183,13 +183,13 @@ class LandingPage extends React.Component
               <path fill="none" d="M0 0h24v24H0V0z"/>
               <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/>
             </svg>
-            Find it on <b>Github</b>
+            Find it on <b>GitHub</b>
           </a>
         </div>
 
         <div className="subtitle-container">
           <div className="subtitle">
-            &copy; 2018 University of California, San Diego
+            {"&copy; 2018 University of California, San Diego. All rights reserved."}
           </div>
           <div className="subtitle">
             <label>{"Thank you for reading me! Stay amazing!"}</label>
