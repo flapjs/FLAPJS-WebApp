6842a8d330fd3cc6d514cefaf90e6ea5c240b4c8
Andrew <andykuo1supergreen@gmail.com>
2018-08-19 03:35:57 -0700

    Back out of directory for cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,7 +6,7 @@
 //Must stay in service worker to trigger update
 //Must also NEVER rename the file
 const CONFIG = {
-  version: '0.0.0a',
+  version: '0.0.0b',
   //TODO: For production mode, change this to false!
   networkPriority: true,
   //TODO: For production mode, change this to false!
