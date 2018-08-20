7e38755b87e7725191e6733468e21c7360e5cadf
Xuanqiang Zhao <vsastray@outlook.com>
2018-08-20 16:53:30 -0700

    transition function

---
+++ src/app/content/drawer/panels/overview/transitionfunction/TransitionFunction.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './TransitionFunction.css';
+
+import InfoBlock from '../infoblock/InfoBlock';
+
+class TransitionFunction extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const machineBuilder = this.props.machineBuilder;
+    const machine = machineBuilder.getMachine();
+    const states = machine.getStates();
+    const alphabet = machine.getAlphabet().sort();
+    let isNFA = machineBuilder.getMachineType() == "NFA";
+    return(
+        <InfoBlock title="Transition Function">
+          <div className="transitionfunction-container">
+            <table className="transitionfunction-table">
+              <tbody>
+                <tr>
+                  <th>Q&#10005;&Sigma;</th>
+                  <th>{isNFA ? "\u2118(Q)" : "Q"}</th>
+                </tr>
+                {
+                  states.map((state, i) => {
+                    return alphabet.map((symbol, j) => {
+                      let className = "";
+                      let trans = machine.doTransition(state, symbol);
+                      if(isNFA && !trans.length) return;
+                      if(!isNFA && !trans.length) {
+                        className = "error";
+                        trans = "-";
+                      }
+                      if(!isNFA && trans.length > 1) {
+                        className = "error";
+                        trans = "{" + trans + "}"
+                      }
+                      trans = isNFA ? "{" + trans + "}" : "" + trans;
+                      return <tr key={""+state+symbol}>
+                          <td>{"(" + state + "," + symbol + ")"}</td>
+                          <td className={className}>{trans}</td>
+                        </tr>
+                    })
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+        </InfoBlock>
+    );
+  }
+}
+
+export default TransitionFunction;
