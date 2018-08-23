df95d7710a9a2e27e4ad206833e7c416b1b9543b
l1lang <langlixuan1@gmail.com>
2018-08-23 14:44:10 -0700

    add autolayout

--- src/app/util/AutoLayout.js
+++ src/app/util/AutoLayout.js
@@ -7,7 +7,17 @@ class AutoLayout
 
   applyLayout(graph)
   {
-    //Apply the layout
+    //handle for too much nodes case
+    const numOfNode = graph.nodes.length;
+    const radius = (numOfNode/5) * 40 + 10;
+    const degree = 2*3.14/numOfNode;
+    let currentStep = 0;
+    for(let i = graph.nodes.length - 1; i >= 0; i--)
+    {
+      graph.nodes[i].x = Math.sin(degree*currentStep)*radius;
+      graph.nodes[i].y = Math.cos(degree*currentStep)*radius;
+      currentStep += 1;
+    }
   }
 }
-export default AutoLayout;
+export default new AutoLayout();
