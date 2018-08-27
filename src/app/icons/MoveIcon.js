0f2255a1829adc2dd8cd25fc6b89fb140b14eda4
Xuanqiang Zhao <vsastray@outlook.com>
2018-08-27 12:37:00 -0700

    move icon

---
+++ src/app/icons/MoveIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MoveIcon extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
+    return <svg className="move-icon" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="#000000" d="M13,6V11H18V7.75L22.25,12L18,16.25V13H13V18H16.25L12,22.25L7.75,18H11V13H6V16.25L1.75,12L6,7.75V11H11V6H7.75L12,1.75L16.25,6H13Z" />
+    </svg>;
+  }
+}
+
+export default MoveIcon;
