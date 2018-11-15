cf7a69d15d93664a52bbbc12333f0e6f5e76b68f
Andrew <andykuo1supergreen@gmail.com>
2018-11-15 12:30:38 -0800

    Fixed missing default empty transition on exit label editor

--- src/app/content/viewport/LabelEditor.js
+++ src/app/content/viewport/LabelEditor.js
@@ -120,7 +120,8 @@ class LabelEditor extends React.Component
 
   onSubmit(newValue, prevValue)
   {
-    if (newValue != prevValue)
+    //If the value has changed or the value remained empty...
+    if (newValue != prevValue || newValue.length <= 0)
     {
       this.closeEditor(true);
     }
