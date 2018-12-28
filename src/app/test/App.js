c66f2bfb4c27e23e4bd946896063aca7324a4b45
Andrew <andykuo1supergreen@gmail.com>
2018-12-28 07:22:23 -0800

    Moved drawer to drawer view

--- src/app/test/App.js
+++ src/app/test/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import DrawerView from './DrawerView.js';
+import DrawerView, { DRAWER_SIDE_RIGHT, DRAWER_SIDE_BOTTOM, DRAWER_BAR_DIRECTION_VERTICAL, DRAWER_BAR_DIRECTION_HORIZONTAL } from './DrawerView.js';
 import Toolbar from './Toolbar.js';
 import ToolbarButton from './ToolbarButton.js';
 
@@ -12,11 +12,16 @@ class App extends React.Component
   constructor(props)
   {
     super(props);
+
+    this._mediaQuerySmallWidthList = window.matchMedia("only screen and (max-width: 400px)");
+    this._mediaQuerySmallHeightList = window.matchMedia("only screen and (min-height: 400px)");
   }
 
   //Override
   render()
   {
+    const hasSmallWidth = this._mediaQuerySmallWidthList.matches;
+    const hasSmallHeight = this._mediaQuerySmallHeightList.matches;
     return (
       <div className="app-container">
         <div className={"app-bar"}>
@@ -27,7 +32,9 @@ class App extends React.Component
             <ToolbarButton title="Other" iconClass={Icon}/>
           </div>
         </div>
-        <DrawerView>
+        <DrawerView
+          side={hasSmallWidth ? DRAWER_SIDE_BOTTOM : DRAWER_SIDE_RIGHT}
+          direction={hasSmallHeight ? DRAWER_BAR_DIRECTION_VERTICAL : DRAWER_BAR_DIRECTION_HORIZONTAL}>
         </DrawerView>
       </div>
     );
--- src/app/test/DrawerView.js
+++ src/app/test/DrawerView.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Style from './DrawerView.css';
 
-import Drawer, { DRAWER_SIDE_RIGHT, DRAWER_SIDE_BOTTOM, DRAWER_BAR_DIRECTION_VERTICAL, DRAWER_BAR_DIRECTION_HORIZONTAL } from './Drawer.js';
+import DrawerPanelView from './DrawerPanelView.js';
+import Icon from './Icon.js';
+
+const DRAWER_WIDTH_CSSVAR = "--drawer-width";
+const DRAWER_HANDLE_DRAG_OFFSET = 6;
+const DRAWER_HANDLE_MIN_SIZE_BUFFER = 32;
+const DRAWER_HANDLE_MAX_SIZE_BUFFER = 128;
+const DRAWER_MIN_WIDTH = 200;
+const DRAWER_SHOULD_HIDE_CONTENT_ON_RESIZE = false;
+
+export const DRAWER_WIDTH_TYPE_FULL = "full";
+export const DRAWER_WIDTH_TYPE_MIN = "min";
+export const DRAWER_SIDE_BOTTOM = "bottom";
+export const DRAWER_SIDE_RIGHT = "right";
+export const DRAWER_BAR_DIRECTION_VERTICAL = "vertical";
+export const DRAWER_BAR_DIRECTION_HORIZONTAL = "horizontal";
 
 class DrawerView extends React.Component
 {
@@ -9,33 +24,100 @@ class DrawerView extends React.Component
   {
     super(props);
 
-    this.drawerComponent = null;
+    this.drawerElement = null;
 
     this.state = {
-      drawerOpen: false
+      open: false
     };
 
-    this._mediaQuerySmallWidthList = window.matchMedia("only screen and (max-width: 400px)");
-    this._mediaQuerySmallHeightList = window.matchMedia("only screen and (min-height: 400px)");
+    this._handlingGrab = false;
+    this._isfull = false;
+    this._hasintent = false;
+    this._prevWidth = DRAWER_MIN_WIDTH;
 
+    this.onDrawerHandleGrab = this.onDrawerHandleGrab.bind(this);
+    this.onDrawerHandleRelease = this.onDrawerHandleRelease.bind(this);
+    this.onDrawerHandleMove = this.onDrawerHandleMove.bind(this);
     this.onDrawerExpand = this.onDrawerExpand.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
+  }
+
+  //Override
+  componentDidMount()
+  {
+    window.addEventListener('resize', this.onWindowResize);
+  }
+
+  //Override
+  componentWillUnmount()
+  {
+    window.removeEventListener('resize', this.onWindowResize);
   }
 
   openDrawer(fullscreen=false)
   {
-    this.setState({ drawerOpen: true });
+    this.setState({ open: true });
   }
 
   closeDrawer()
   {
-    this.setState({ drawerOpen: false });
+    this.setState({ open: false });
   }
 
   toggleDrawer()
   {
     this.setState((prev, props) => {
-      return { drawerOpen: !prev.drawerOpen };
+      return { open: !prev.open };
     });
+  }
+
+  setDrawerWidth(value)
+  {
+    if (!this.drawerElement) return;
+
+    this._hasintent = true;
+
+    const drawerSide = this.props.side;
+    const isDrawerSideBottom = drawerSide === DRAWER_SIDE_BOTTOM;
+    const documentSize = isDrawerSideBottom ?
+      document.documentElement.clientHeight - 80 - 2:
+      document.documentElement.clientWidth;
+    const minSize = DRAWER_MIN_WIDTH;
+
+    if (typeof value === 'string')
+    {
+      switch(value)
+      {
+        case DRAWER_WIDTH_TYPE_FULL:
+          this.drawerElement.style.setProperty(DRAWER_WIDTH_CSSVAR, documentSize + "px");
+          this._isfull = true;
+          break;
+        case DRAWER_WIDTH_TYPE_MIN:
+          this.drawerElement.style.setProperty(DRAWER_WIDTH_CSSVAR, minSize + "px");
+          this._isfull = false;
+          break;
+        default:
+          throw new Error("Trying to set drawer width to invalid string");
+      }
+    }
+    else if (typeof value === 'number')
+    {
+      if (value < minSize + DRAWER_HANDLE_MIN_SIZE_BUFFER) value = minSize;
+      if (value > documentSize - DRAWER_HANDLE_MAX_SIZE_BUFFER)
+      {
+        value = documentSize;
+        this._isfull = true;
+      }
+      else
+      {
+        this._isfull = false;
+      }
+      this.drawerElement.style.setProperty(DRAWER_WIDTH_CSSVAR, value + "px");
+    }
+    else
+    {
+      throw new Error("Trying to set drawer width to unknown value");
+    }
   }
 
   onDrawerExpand()
@@ -43,24 +125,124 @@ class DrawerView extends React.Component
     this.toggleDrawer();
   }
 
+  onWindowResize(e)
+  {
+    if (!this.drawerElement) return;
+
+    const drawerSide = this.props.side;
+    const isDrawerSideBottom = drawerSide === DRAWER_SIDE_BOTTOM;
+    const documentSize = isDrawerSideBottom ?
+      document.documentElement.clientHeight - 80 - 2:
+      document.documentElement.clientWidth;
+
+    if (this._isfull)
+    {
+      if (this._hasintent) return;
+      if (this._prevWidth + DRAWER_HANDLE_MAX_SIZE_BUFFER < documentSize)
+      {
+        this._isfull = false;
+        this.drawerElement.style.setProperty(DRAWER_WIDTH_CSSVAR, this._prevWidth + "px");
+      }
+    }
+    else
+    {
+      const drawerSize = parseInt(window.getComputedStyle(this.drawerElement).getPropertyValue(DRAWER_WIDTH_CSSVAR).replace(/\D/g, ''));
+      if (drawerSize + DRAWER_HANDLE_MAX_SIZE_BUFFER > documentSize)
+      {
+        this._isfull = true;
+        this._hasintent = false;
+        this.drawerElement.style.setProperty(DRAWER_WIDTH_CSSVAR, drawerSize + "px");
+      }
+    }
+  }
+
+  onDrawerHandleGrab(e)
+  {
+    if (!this._handlingGrab)
+    {
+      this._handlingGrab = true;
+      this._prevWidth = parseInt(window.getComputedStyle(this.drawerElement).getPropertyValue(DRAWER_WIDTH_CSSVAR).replace(/\D/g, ''));
+      document.addEventListener("mouseup", this.onDrawerHandleRelease);
+      document.addEventListener("mousemove", this.onDrawerHandleMove);
+    }
+    else
+    {
+      this.onDrawerHandleRelease(e);
+    }
+  }
+
+  onDrawerHandleRelease(e)
+  {
+    if (this._handlingGrab)
+    {
+      document.removeEventListener("mouseup", this.onDrawerHandleRelease);
+      document.removeEventListener("mousemove", this.onDrawerHandleMove);
+      this._handlingGrab = false;
+      this._prevWidth = parseInt(window.getComputedStyle(this.drawerElement).getPropertyValue(DRAWER_WIDTH_CSSVAR).replace(/\D/g, ''));
+    }
+  }
+
+  onDrawerHandleMove(e)
+  {
+    const drawerSide = this.props.side;
+    const isDrawerSideBottom = drawerSide === DRAWER_SIDE_BOTTOM;
+    const documentSize = isDrawerSideBottom ?
+      document.documentElement.clientHeight :
+      document.documentElement.clientWidth;
+    const pointerValue = isDrawerSideBottom ? e.clientY : e.clientX;
+    this.setDrawerWidth(documentSize - pointerValue + DRAWER_HANDLE_DRAG_OFFSET);
+  }
+
   //Override
   render()
   {
-    const hasSmallWidth = this._mediaQuerySmallWidthList.matches;
-    const hasSmallHeight = this._mediaQuerySmallHeightList.matches;
+    const drawerSide = this.props.side || DRAWER_SIDE_RIGHT;
+    const drawerDirection = this.props.direction || DRAWER_BAR_DIRECTION_HORIZONTAL;
+
+    const isDrawerOpen = this.state.open;
+    //Assumes that parent container has flex and flex-direction: column for bottom, row for right.
+    const isDrawerSideBottom = drawerSide === DRAWER_SIDE_BOTTOM;
+    const shouldDrawerBarSideways = !isDrawerSideBottom && (!isDrawerOpen || drawerDirection === DRAWER_BAR_DIRECTION_VERTICAL);
+    const showDrawerHandle = isDrawerOpen || this._handlingGrab;
+    const shouldDrawerOpenFull = this._isfull;
+    const shouldHideDrawerContent = (DRAWER_SHOULD_HIDE_CONTENT_ON_RESIZE && this._handlingGrab) || !isDrawerOpen;
+
     return (
       <div id={this.props.id}
       className={Style.app_content +
-        (hasSmallWidth ? " column " : "") +
+        (isDrawerSideBottom ? " column " : "") +
         (" " + this.props.className)}
       style={this.props.style}>
         <div className={Style.app_viewport}>
         </div>
-        <Drawer ref={ref=>this.drawerComponent=ref}
-          open={this.state.drawerOpen}
-          side={hasSmallWidth ? DRAWER_SIDE_BOTTOM : DRAWER_SIDE_RIGHT}
-          direction={hasSmallHeight ? DRAWER_BAR_DIRECTION_VERTICAL : DRAWER_BAR_DIRECTION_HORIZONTAL}
-          onExpand={this.onDrawerExpand}/>
+        <div ref={ref=>this.drawerElement=ref} className={
+          Style.app_drawer +
+          (isDrawerOpen ? " open " : "") +
+          (shouldDrawerBarSideways ? " drawer-bar-sideways " : "") +
+          (isDrawerSideBottom ? " drawer-side-bottom " : "") +
+          (shouldDrawerOpenFull ? " full " : "") +
+          (shouldHideDrawerContent ? " hide " : "")
+        }>
+          <div className={Style.drawer_handle + (showDrawerHandle ? " show " : "")} onMouseDown={this.onDrawerHandleGrab}>
+            <span>{isDrawerSideBottom ? "\uFF1D" : "||"}</span>
+          </div>
+          <div className={Style.drawer_content}>
+            <nav className={Style.drawer_content_bar}>
+              <a className={Style.drawer_tab_expander} onClick={this.onDrawerExpand}>
+                <Icon/>
+              </a>
+              <a className={Style.drawer_tab}><label>Start</label></a>
+              <a className={Style.drawer_tab}><label>Middle-ish</label></a>
+              <a className={Style.drawer_tab}><label>End</label></a>
+            </nav>
+            <div className={Style.drawer_panel_container}>
+              <div className={Style.drawer_content_panel}>
+                <DrawerPanelView>
+                </DrawerPanelView>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
