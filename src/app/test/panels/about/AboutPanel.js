2dc25c79e13d43cdbc7d980529d72dd534244fd4
Andrew <andykuo1supergreen@gmail.com>
2019-01-02 12:01:07 -0800

    Removed Meet the Team from About panel

--- src/app/test/panels/about/AboutPanel.js
+++ src/app/test/panels/about/AboutPanel.js
@@ -24,28 +24,6 @@ class AboutPanel extends React.Component
         <div className={Style.panel_content}>
           <p>{"Brought to you with \u2764 by the Flap.js team."}</p>
           <p>{"<- Tap on a tab to begin!"}</p>
-          <PanelSection title={"Meet the Team"}>
-            <h2>The Team</h2>
-            <ul>
-              <li>{"David Osuna"}</li>
-            </ul>
-            <h2>The OG Team</h2>
-            <ul>
-              <li>{"David Osuna"}</li>
-            </ul>
-            <h2>Past Teams</h2>
-            <ul>
-              <li>{"David Osuna"}</li>
-            </ul>
-          </PanelSection>
-          <PanelSection title={"Special Thanks"}>
-            <ul>
-              <li>{"To the UCSD Summer Internship Program for gathering us together,"}</li>
-              <li>{"To the CSE Building Room Reservation people for their hard work,"}</li>
-              <li>{"To our friends and family,"}</li>
-              <li>{"And finally, to you. Thank you for using Flap.js!"}</li>
-            </ul>
-          </PanelSection>
         </div>
       </div>
     );
