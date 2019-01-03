84cd527c59bda4cb3c6e0a6580661b2b9067e1fa
Andrew <andykuo1supergreen@gmail.com>
2019-01-03 07:06:05 -0800

    Added exit button for experimental mode

--- src/app/experimental/menus/option/OptionPanel.js
+++ src/app/experimental/menus/option/OptionPanel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Style from './OptionPanel.css';
 
+import LocalSave from 'system/localsave/LocalSave.js';
+
 import PanelSection from 'experimental/panels/PanelSection.js';
 
 class OptionPanel extends React.Component
@@ -23,10 +25,27 @@ class OptionPanel extends React.Component
           <h1>{I18N.toString("component.options.title")}</h1>
         </div>
         <div className={Style.panel_content}>
+
           <PanelSection title={"Customize stuff"}>
+            <p>If only there was cool stuff here...</p>
           </PanelSection>
+
           <PanelSection title={"What?"}>
           </PanelSection>
+
+          <button onClick={() => {
+            if (window.confirm("This will clear any cached or saved data. Are you sure you want to continue?"))
+            {
+              try { LocalSave.terminate(); }
+              catch(e){/* Ignore if it fails. */}
+              LocalSave.clearStorage();
+              
+              window.alert("It's done! Restart to apply changes!");
+            }
+          }}>
+            {"Get out of Experimental Mode"}
+          </button>
+
         </div>
       </div>
     );
