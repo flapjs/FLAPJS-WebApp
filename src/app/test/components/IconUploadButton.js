fd770575644efcffe4ba6cc696d16c9d7dd76b9d
Andrew <andykuo1supergreen@gmail.com>
2019-01-03 00:12:49 -0800

    Implemented IconUploadButton

---
+++ src/app/test/components/IconUploadButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Style from './IconUploadButton.css';
+
+import IconButton from './IconButton.js';
+
+class IconUploadButton extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.onFileUpload = this.onFileUpload.bind(this);
+  }
+
+  onFileUpload(e)
+  {
+    const files = e.target.files;
+    if (files.length > 0)
+    {
+      if (this.props.onUpload) this.props.onUpload(files[0]);
+
+      //Makes sure you can upload the same file again.
+      e.target.value = "";
+    }
+  }
+
+  //Override
+  render()
+  {
+    return (
+      <IconButton id={this.props.id}
+        className={Style.upload_button +
+          " " + this.props.className}
+        style={this.props.style}
+        title={this.props.title}
+        disabled={this.props.disabled}>
+        <input type="file" name="import"
+          className={Style.upload_input}
+          accept={this.props.accept}
+          onChange={this.onFileUpload}/>
+        {this.props.children}
+      </IconButton>
+    );
+  }
+}
+
+export default IconUploadButton;
--- src/app/test/toolbar/ToolbarUploadButton.js
+++ src/app/test/toolbar/ToolbarUploadButton.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import ToolbarButtonStyle from './ToolbarButton.css';
-import Style from './ToolbarUploadButton.css';
 
-import IconButton from 'test/components/IconButton.js';
+import ToolbarButtonStyle from './ToolbarButton.css';
+import IconUploadButton from 'test/components/IconUploadButton.js';
 
 class ToolbarUploadButton extends React.Component
 {
   constructor(props)
   {
     super(props);
-
-    this.onFileUpload = this.onFileUpload.bind(this);
-  }
-
-  onFileUpload(e)
-  {
-    const files = e.target.files;
-    if (files.length > 0)
-    {
-      if (this.props.onUpload) this.props.onUpload(files[0]);
-
-      //Makes sure you can upload the same file again.
-      e.target.value = "";
-    }
   }
 
   //Override
@@ -30,19 +15,16 @@ class ToolbarUploadButton extends React.Component
   {
     const IconClass = this.props.icon;
     return (
-      <IconButton id={this.props.id}
+      <IconUploadButton id={this.props.id}
         className={ToolbarButtonStyle.toolbar_button +
-          " " + Style.upload_button +
           " " + this.props.className}
         style={this.props.style}
         title={this.props.title}
-        disabled={this.props.disabled}>
-        <input type="file" name="import"
-          className={Style.upload_input}
-          accept={this.props.accept}
-          onChange={this.onFileUpload}/>
+        disabled={this.props.disabled}
+        accept={this.props.accept}
+        onUpload={this.props.onUpload}>
         <IconClass/>
-      </IconButton>
+      </IconUploadButton>
     );
   }
 }
