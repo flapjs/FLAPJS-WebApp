16206bee742b60af8e9c83732d4674982a0e7d68
Andrew <andykuo1supergreen@gmail.com>
2019-01-14 18:44:36 -0800

    Change changelog

--- src/app/changelog.js
+++ src/app/changelog.js
@@ -1,11 +1,20 @@
-//Version 0.3.1
+//Version 0.3.2
 const changeLog = [
-  "FIXED: Renaming an uploaded file does not download file with new name.",
-  "This is our first user-reported bug! Hooray!",
-  "Thank you and have a hug whoever you are <3."
+  "FIXED: Allow cross-platform bundle building.",
+  "FIXED: Corrected 'development' mode while in production.",
+  "Disabled service worker while in 'development' mode."
 ];
-const changeVersion = process.env.VERSION;
 
+/*
+  //Version 0.3.1
+  const changeLog = [
+    "FIXED: Renaming an uploaded file does not download file with new name.",
+    "This is our first user-reported bug! Hooray!",
+    "Thank you and have a hug whoever you are <3."
+  ];
+*/
+
+const changeVersion = process.env.VERSION;
 const result = {
   show: changeLog && changeLog.length > 0,
   log: "\nChangelog v" + changeVersion + ":\n - " + changeLog.join("\n - "),
