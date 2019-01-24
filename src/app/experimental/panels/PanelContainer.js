9684f4818b791297e505da7684d6c0a8b7549db4
Andrew <andykuo1supergreen@gmail.com>
2019-01-23 16:21:29 -0800

    Added panel components

---
+++ src/app/experimental/panels/PanelContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Style from './PanelContainer.css';
+
+class PanelContainer extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  //Override
+  render()
+  {
+    const title = this.props.title;
+
+    return (
+      <div id={this.props.id}
+        className={Style.panel_container +
+          " " + this.props.className}
+        style={this.props.style}>
+        <div className={Style.panel_title}>
+          <h1>{title}</h1>
+        </div>
+        <div className={Style.panel_content}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PanelContainer;
---
+++ src/app/experimental/panels/PanelDivider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Style from './PanelDivider.css';
+
+class PanelDivider extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  //Override
+  render()
+  {
+    return (
+      <div id={this.props.id}
+        className={Style.panel_divider +
+          " " + this.props.className}
+        style={this.props.style}>
+      </div>
+    );
+  }
+}
+
+export default PanelDivider;
