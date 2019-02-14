6080e68a198ea91f173ebeccec9c046b27d681f2
Andrew <andykuo1supergreen@gmail.com>
2019-02-14 11:00:11 -0800

    Changed RE to use the new FSA module

---
+++ src/app/modules/fsa2/machine/RE.js
@@ -0,0 +1,163 @@
+import GraphElement from 'graph/GraphElement.js';
+import { guid, stringHash } from 'util/MathHelper.js';
+
+export const EMPTY = "\u03B5";
+export const CONCAT = ".";
+export const UNION = "|";
+export const KLEENE = "*";
+
+class RE
+{
+  constructor()
+  {
+    this._expression = "";
+
+    this._errors = [];
+  }
+
+  /**
+   * Performs a shallow copy of the 2 machines. Any changes to a state will be
+   * reflected in both. However, changes to transitions, alphabet, and final
+   * states will not propagate.
+   */
+  copy(re)
+  {
+    //You are already yourself, don't copy nothing.
+    if (re === this) return;
+
+    //Make room for the copy...
+    this.clear();
+
+    this._expression = re._expression;
+
+    //Copy errors
+    for(const error of fsa._errors)
+    {
+      //WARNING: if the error's store state objects, they need to be redirected to the copies
+      this._errors.push(error);
+    }
+  }
+
+  clear()
+  {
+    this._expression = "";
+
+    this._errors.length = 0;
+  }
+
+  validate()
+  {
+    //Reset errors
+    this._errors.length = 0;
+
+    try
+    {
+      if (this.isExpressionValid())
+      {
+        return true;
+      }
+    }
+    catch (e)
+    {
+      this._errors.push(e);
+    }
+    
+    return false;
+  }
+  isValid() { return this._errors.length == 0; }
+  getErrors() { return this._errors; }
+
+  areParenthesisBalanced() {
+      let count = 0;
+      let expression = this.getExpression();
+	for (let i = 0; i < expression.length; i++) {
+		if (expression.charAt(i) == '(')
+			count++;
+		else if (expression.charAt(i) == ')')
+			count--;
+		if (count < 0)
+			return false;
+	}
+	return count == 0;
+  }
+
+  isExpressionValid() {
+      let expression = this.getExpression();
+	if (expression.length == 0)
+		throw new Error("The expression must be nonempty.");
+	if (!this.areParenthesisBalanced())
+		throw new Error("The parentheses are unbalanced!");
+      switch(expression.charAt(0)) {
+          //Only '(' or a symbol can be the first character
+          case ')':
+          case UNION:
+          case KLEENE:
+          case CONCAT:
+              throw new Error("Operators are poorly formatted.");
+      }
+      for (let i = 1; i < expression.length; i++) {
+		let currChar = expression.charAt(i);
+		let prevChar = expression.charAt(i - 1);
+		switch (currChar) {
+  			case UNION:
+              case CONCAT:
+                  // UNION can't be the last character
+  				if (i == expression.length - 1)
+  					throw new UnsupportedOperationException(
+  							"Operators are poorly formatted.");
+  			case ')':
+  			case KLEENE:
+                  // Must be preceded with a symbol
+  				if (prevChar == '(' || prevChar == UNION || prevChar == CONCAT)
+  					throw new Error("Operators are poorly formatted.");
+  				break;
+  			case EMPTY:
+  				if (prevChar != '(' && prevChar != UNION)
+  					throw new Error("Epsilon must not cat with anything else.");
+  				if (i == expression.length - 1)
+  					break;
+  				nextChar = string.charAt(i + 1);
+  				if (nextChar != ')' && nextChar != UNION && nextChar != KLEENE)
+  					throw new Error("Epsilon must not cat with anything else.");
+  				break;
+		}
+	}
+	return expression;
+  }
+
+  insertConcatSymbols(){
+      let result="";
+      let expression = this.getExpression();
+      for(let i=0; i < expression.length; i++){
+          let currChar = expression.charAt(i);
+          result += currChar;
+          if( i + 1 < expression.length){
+              let nextChar = expression.charAt(i + 1);
+              if(currChar != '(' && currChar != UNION &&
+                  nextChar != ')' && nextChar != UNION && nextChar != KLEENE){
+                  result+=CONCAT;
+              }
+          }
+      }
+      this.setExpression(result);
+  }
+
+
+  setExpression(expression)
+  {
+    this._expression = expression;
+    return this;
+  }
+
+  getExpression()
+  {
+    return this._expression;
+  }
+
+  getHashCode()
+  {
+    return stringHash(this._expression);
+  }
+}
+
+export default RE;
---
+++ src/app/modules/fsa2/machine/util/ConvertRE.js
@@ -0,0 +1,202 @@
+import FSA, {EMPTY_SYMBOL} from '../FSA.js';
+import {EMPTY,
+  CONCAT,
+	UNION,
+	KLEENE} from '../RE.js';
+
+const SRC = 0;
+const SYMBOL = 1;
+const DST = 2;
+
+// Return NFA representation of the input regular expression
+// Construction of NFA is done using Thompson's algorithm
+export function convertToNFA(re)
+{
+	const parser = new RegexParser();
+	parser.parseRegex(re);
+	return ASTtoNFA(parser.rootNode);
+}
+
+function ASTtoNFA(astNode)
+{
+	//Base case, terminal nodes are characters in the alphabet
+	if (astNode.isTerminal())
+	{
+		return character(astNode.getSymbol());
+	}
+	switch (astNode.getSymbol())
+	{
+	case KLEENE:
+		return kleene(ASTtoNFA(astNode._children[0]));
+	case CONCAT:
+		return concat(ASTtoNFA(astNode._children[0]), ASTtoNFA(astNode._children[1]));
+	case UNION:
+		return or(ASTtoNFA(astNode._children[0]), ASTtoNFA(astNode._children[1]));
+	case '(':
+		return ASTtoNFA(astNode._children[0])
+	default:
+		throw new Error("You've got a weird node in the AST tree with symbol " + astNode.getSymbol());
+	}
+}
+
+// For a symbol of the alphabet, the NFA is two states, a start and a finish state,
+// with the transition being the symbol.
+function character(symbol)
+{
+	const result = new FSA(false);
+	const state0 = result.createState("q0");
+	const state1 = result.createState("q1");
+	result.addTransition(state0, state1, symbol);
+	result.setStartState(state0);
+	result.setFinalState(state1);
+	return result;
+}
+
+function concat(a, b)
+{
+	const result = new FSA(false);
+  let stateIndex = 0;
+
+  let aStateMap = new Map();
+  let firstAState = null;
+  let lastAState = null;
+  for(const state of a.getStates())
+  {
+    let newState = result.createState("q" + (stateIndex++));
+    aStateMap.set(state, newState);
+    if (firstAState === null) firstAState = newState;
+    lastAState = newState;
+  }
+
+  let bStateMap = new Map();
+  let firstBState = null;
+  let lastBState = null;
+  for(const state of b.getStates())
+  {
+    let newState = result.createState("q" + (stateIndex++));
+    bStateMap.set(state, newState);
+    if (firstBState === null) firstBState = newState;
+    lastBState = newState;
+  }
+
+	const aTransitions = a.getTransitions();
+	for (const transition of aTransitions)
+	{
+    const newFromState = aStateMap.get(transition[SRC]);
+    const newToState = aStateMap.get(transition[DST]);
+		result.addTransition(newFromState, newToState, transition[SYMBOL]);
+	}
+
+  const aFinalState = a.getFinalStates()[0];
+  const newFinalState = aStateMap.get(aFinalState);
+  result.addTransition(newFinalState, firstBState, EMPTY_SYMBOL);
+
+	const bTransitions = b.getTransitions();
+	for (const transition of bTransitions)
+	{
+    const newFromState = bStateMap.get(transition[SRC]);
+    const newToState = bStateMap.get(transition[DST]);
+		result.addTransition(newFromState, newToState, transition[SYMBOL]);
+	}
+
+	result.setStartState(firstAState);
+	result.setFinalState(lastBState);
+	return result;
+}
+
+function kleene(a)
+{
+	const result = new FSA(false);
+  let stateIndex = 0;
+
+  const stateMap = new Map();
+
+  const firstState = result.createState("q" + (stateIndex++));
+  let firstAState = null;
+  let lastAState = null;
+  for(const state of a.getStates())
+  {
+    let newState = result.createState("q" + (stateIndex++));
+    stateMap.set(state, newState);
+
+    if (firstAState === null) firstAState = newState;
+    lastAState = newState;
+  }
+  const lastState = result.createState("q" + (stateIndex++));
+
+  result.addTransition(firstState, firstAState, EMPTY_SYMBOL);
+
+  const aTransitions = a.getTransitions();
+	for (const transition of aTransitions)
+	{
+    const newFromState = aStateMap.get(transition[SRC]);
+    const newToState = aStateMap.get(transition[DST]);
+		result.addTransition(newFromState, newToState, transition[SYMBOL]);
+	}
+
+	result.addTransition(lastAState, lastState, EMPTY_SYMBOL);
+	result.addTransition(lastAState, firstAState, EMPTY_SYMBOL);
+	result.addTransition(firstState, lastState, EMPTY);
+
+	result.setStartState(firstState);
+	result.setFinalState(lastState);
+	return result;
+}
+
+function or(a, b)
+{
+	const result = new FSA(false);
+  let stateIndex = 0;
+
+  const firstState = result.createState("q" + (stateIndex++));
+
+  let aStateMap = new Map();
+  let firstAState = null;
+  let lastAState = null;
+  for(const state of a.getStates())
+  {
+    let newState = result.createState("q" + (stateIndex++));
+    aStateMap.set(state, newState);
+    if (firstAState === null) firstAState = newState;
+    lastAState = newState;
+  }
+
+  let bStateMap = new Map();
+  let firstBState = null;
+  let lastBState = null;
+  for(const state of b.getStates())
+  {
+    let newState = result.createState("q" + (stateIndex++));
+    bStateMap.set(state, newState);
+    if (firstBState === null) firstBState = newState;
+    lastBState = newState;
+  }
+
+  const lastState = result.createState("q" + (stateIndex++));
+
+  //A machine
+	result.addTransition(firstState, firstAState, EMPTY_SYMBOL);
+  const aTransitions = a.getTransitions();
+	for (const transition of aTransitions)
+	{
+    const newFromState = aStateMap.get(transition[SRC]);
+    const newToState = aStateMap.get(transition[DST]);
+    result.addTransition(newFromState, newToState, transition[SYMBOL]);
+	}
+	result.addTransition(lastAState, lastState, EMPTY_SYMBOL);
+
+  //B machine
+	result.addTransition(firstState, firstBState, EMPTY_SYMBOL);
+  const bTransitions = b.getTransitions();
+	for (const transition of bTransitions)
+	{
+    const newFromState = bStateMap.get(transition[SRC]);
+    const newToState = bStateMap.get(transition[DST]);
+    result.addTransition(newFromState, newToState, transition[SYMBOL]);
+	}
+	result.addTransition(lastBState, lastState, EMPTY_SYMBOL);
+
+	result.setStartState(firstState);
+	result.setFinalState(lastState);
+	return result;
+}
