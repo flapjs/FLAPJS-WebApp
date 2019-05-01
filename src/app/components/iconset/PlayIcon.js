5b44910ee6aba534cb0d70f1a3ff0adf45d39d5d
Andrew <andykuo1supergreen@gmail.com>
2019-04-30 23:02:58 -0700

    Fixed linting errors for iconset

--- src/app/components/iconset/PlayIcon.js
+++ src/app/components/iconset/PlayIcon.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 class Icon extends React.Component
 {
-  constructor(props) { super(props); }
+    constructor(props) { super(props); }
 
-  /** @override */
-  render()
-  {
-    return (
-      <svg id={this.props.id} className={this.props.className} style={this.props.style}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24" height="24" viewBox="0 0 24 24">
-        <path d="M8 5v14l11-7z"/>
-      </svg>
-    );
-  }
+    /** @override */
+    render()
+    {
+        return (
+            <svg id={this.props.id} className={this.props.className} style={this.props.style}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24" viewBox="0 0 24 24">
+                <path d="M8 5v14l11-7z"/>
+            </svg>
+        );
+    }
 }
 export default Icon;
--- src/app/graph/renderer/GraphEdgeRenderer.js
+++ src/app/graph/renderer/GraphEdgeRenderer.js
@@ -8,33 +8,33 @@ const GRAPH_EDGE_LABEL_STYLE_NAME = "graph-edge-label";
 
 class GraphEdgeRenderer extends React.Component
 {
-  constructor(props)
-  {
-    super(props);
-  }
+    constructor(props)
+    {
+        super(props);
+    }
 
-  /** @override */
-  render()
-  {
-    const edge = this.props.edge;
-    const stroke = this.props.stroke || "#000000";
-    const start = edge.getStartPoint();
-    const end = edge.getEndPoint();
-    const center = edge.getCenterPoint();
-    const label = edge.getEdgeLabel();
-    const edgeDir = edge.getEdgeDirection();
+    /** @override */
+    render()
+    {
+        const edge = this.props.edge;
+        const stroke = this.props.stroke || "#000000";
+        const start = edge.getStartPoint();
+        const end = edge.getEndPoint();
+        const center = edge.getCenterPoint();
+        const label = edge.getEdgeLabel();
+        const edgeDir = edge.getEdgeDirection();
 
-    return <g className={GRAPH_EDGE_CONTAINER_STYLE_NAME}>
-      <EdgeRenderer
-        directed={DIRECTED_NONE}
-        from={start}
-        to={end}
-        center={center}
-        label={label}
-        direction={edgeDir}
-        color={stroke}/>
-    </g>;
-  }
+        return <g className={GRAPH_EDGE_CONTAINER_STYLE_NAME}>
+            <EdgeRenderer
+                directed={DIRECTED_NONE}
+                from={start}
+                to={end}
+                center={center}
+                label={label}
+                direction={edgeDir}
+                color={stroke}/>
+        </g>;
+    }
 }
 
 export default GraphEdgeRenderer;
