fb5f492f52e13a096c3b59e4102b96e7fb9cfca1
Andrew <andykuo1supergreen@gmail.com>
2019-05-01 00:43:11 -0700

    Fixed lint errors for abstract and empty module

--- src/app/modules/empty/EmptyModule.js
+++ src/app/modules/empty/EmptyModule.js
@@ -1,42 +1,42 @@
-const MODULE_NAME = "empty";
-const MODULE_VERSION = "0.0.1";
-const MODULE_LOCALIZED_NAME = "Empty";
+const MODULE_NAME = 'empty';
+const MODULE_VERSION = '0.0.1';
+const MODULE_LOCALIZED_NAME = 'Empty';
 
 class EmptyModule
 {
-  constructor(app)
-  {
-    this._app = app;
-  }
+    constructor(app)
+    {
+        this._app = app;
+    }
 
-  /** @override */
-  initialize(app)
-  {
-  }
+    /** @override */
+    initialize(app)
+    {
+    }
 
-  /** @override */
-  update(app)
-  {
-  }
+    /** @override */
+    update(app)
+    {
+    }
 
-  /** @override */
-  destroy(app)
-  {
-  }
+    /** @override */
+    destroy(app)
+    {
+    }
 
-  /** @override */
-  clear(app)
-  {
-  }
+    /** @override */
+    clear(app)
+    {
+    }
 
-  /** @override */
-  getModuleVersion() { return MODULE_VERSION; }
-  /** @override */
-  getModuleName() { return MODULE_NAME; }
-  /** @override */
-  getLocalizedModuleName() { return MODULE_LOCALIZED_NAME; }
-  /** @override */
-  getApp() { return this._app; }
+    /** @override */
+    getModuleVersion() { return MODULE_VERSION; }
+    /** @override */
+    getModuleName() { return MODULE_NAME; }
+    /** @override */
+    getLocalizedModuleName() { return MODULE_LOCALIZED_NAME; }
+    /** @override */
+    getApp() { return this._app; }
 }
 
 export default EmptyModule;
