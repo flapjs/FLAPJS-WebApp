ef5e7b6f45a240eb2d34f60874e43b30af06b87a
Andrew <andykuo1supergreen@gmail.com>
2019-05-06 14:07:47 -0700

    Added graph2

---
+++ src/app/graph2/NodeGraphParser.js
@@ -0,0 +1,174 @@
+import Parser from 'session/manager/exporter/Parser.js';
+import SemanticVersion from 'util/version/SemanticVersion.js';
+import NodeGraph from './NodeGraph.js';
+
+import GraphNode from 'graph2/element/GraphNode';
+import GraphEdge from 'graph2/element/GraphEdge.js';
+import QuadraticEdge from 'graph2/element/QuadraticEdge.js';
+
+export const VERSION_STRING = "1.0.0";
+export const VERSION = SemanticVersion.parse(VERSION_STRING);
+
+/**
+ * A class that parses and composes NodeGraph's to and from JSON data.
+ */
+class NodeGraphParser extends Parser
+{
+	constructor() { super(); }
+
+	/**
+	 * @override
+	 * @param  {Object} data         		the graph data to parse
+	 * @param  {NodeGraph} [dst=null]		the target to set parsed graph data
+	 * @return {NodeGraph}            	the result in the passed-in dst
+	 */
+	parse(data, dst = null)
+	{
+		if (typeof data !== 'object')
+		{
+			throw new Error("Unable to parse data of non-object type");
+		}
+
+		const dataVersion = SemanticVersion.parse(data['_version'] || "0.0.0");
+		if (!dataVersion.canSupportVersion(VERSION))
+		{
+			throw new Error("Unable to parse data for incompatible version \'" + dataVersion + "\'");
+		}
+
+		const nodeDatas = data['nodes'] || [];
+		const nodeCount = Math.min(nodeDatas.length || 0, data['nodeCount'] || 0);
+		const edgeDatas = data['edges'] || [];
+		const edgeCount = Math.min(edgeDatas.length || 0, data['edgeCount'] || 0);
+		const hasQuad = data['quad'] || false;
+
+		if (!dst)
+		{
+			dst = new NodeGraph(GraphNode, hasQuad ? QuadraticEdge : GraphEdge);
+		}
+		else
+		{
+			dst.clear();
+		}
+
+		const nodeIndices = new Map();
+		for (let i = 0; i < nodeCount; ++i)
+		{
+			const nodeData = nodeDatas[i];
+			if (!nodeData) continue;
+
+			//NOTE: Assumes createNode will maintain order
+			const node = dst.createNode(nodeData['x'] || 0, nodeData['y'] || 0, nodeData['id']);
+			node.setNodeLabel(nodeData['label'] || "");
+
+			nodeIndices.set(i, node);
+		}
+
+		for (let i = 0; i < edgeCount; ++i)
+		{
+			const edgeData = edgeDatas[i];
+			if (!edgeData) continue;
+			const sourceNode = nodeIndices.get(edgeData['from']) || null;
+			//Cannot create source-less edges
+			if (!sourceNode) continue;
+			const destinationNode = nodeIndices.get(edgeData['to']) || null;
+			//NOTE: Assumes createEdge will maintain order
+			const edge = dst.createEdge(sourceNode, destinationNode, edgeData['id']);
+			edge.setEdgeLabel(edgeData['label'] || "");
+
+			if (edge instanceof QuadraticEdge)
+			{
+				const quadData = edgeData['quad'] || {};
+				edge.setQuadraticRadians(quadData['radians'] || 0)
+				edge.setQuadraticLength(quadData['length'] || 0);
+			}
+		}
+
+		return dst;
+	}
+
+	/**
+	 * @override
+	 * @param  {NodeGraph} target     the graph to compose into data
+	 * @param  {Object} [dst=null]    the object to append graph data
+	 * @return {Object}               the result in the passed-in dst
+	 */
+	compose(target, dst = null)
+	{
+		if (!(target instanceof NodeGraph))
+		{
+			throw new Error("Unable to compose target of unknown type");
+		}
+
+		if (!dst) dst = {};
+
+		const graphNodes = target.getNodes() || [];
+		const nodeCount = graphNodes.length || 0;
+		const graphEdges = target.getEdges() || [];
+		const edgeCount = graphEdges.length || 0;
+
+		const nodeDatas = new Array(nodeCount);
+		const nodeIndices = new Map();
+		let node;
+		for (let i = 0; i < nodeCount; ++i)
+		{
+			node = graphNodes[i];
+			if (node)
+			{
+				//NOTE: Assumes node must have an id
+				const elementID = node.getGraphElementID();
+
+				nodeIndices.set(node, i);
+				nodeDatas[i] = {
+					id: elementID,
+					x: node.x || 0, y: node.y || 0,
+					label: node.getNodeLabel() || ""
+				};
+			}
+		}
+
+		let hasQuad = false;
+		const edgeDatas = new Array(edgeCount);
+		let edge;
+		for (let i = 0; i < edgeCount; ++i)
+		{
+			edge = graphEdges[i];
+			if (edge)
+			{
+				//NOTE: Assumes edge must have an id
+				const elementID = edge.getGraphElementID();
+
+				const edgeSource = edge.getEdgeFrom();
+				const edgeDestination = edge.getEdgeTo();
+				const sourceIndex = nodeIndices.has(edgeSource) ? nodeIndices.get(edgeSource) : -1;
+				const destinationIndex = nodeIndices.has(edgeDestination) ? nodeIndices.get(edgeDestination) : -1;
+				edgeDatas[i] = {
+					id: elementID,
+					from: sourceIndex,
+					to: destinationIndex,
+					label: edge.getEdgeLabel() || ""
+				};
+
+				if (edge instanceof QuadraticEdge)
+				{
+					const quad = edge.getQuadratic() || {};
+					hasQuad = true;
+					edgeDatas[i]['quad'] = {
+						radians: quad['radians'] || 0,
+						length: quad['length'] || 0
+					};
+				}
+			}
+		}
+
+		dst['_version'] = VERSION_STRING;
+		dst['nodeCount'] = nodeCount;
+		dst['nodes'] = nodeDatas;
+		dst['edgeCount'] = edgeCount;
+		dst['edges'] = edgeDatas;
+		dst['hasQuad'] = hasQuad;
+
+		return dst;
+	}
+}
+
+export default NodeGraphParser;
---
+++ src/app/graph2/controller/InputController.js
@@ -0,0 +1,195 @@
+import GraphNodeInputHandler from '../inputs/GraphNodeInputHandler.js';
+import GraphEdgeInputHandler from '../inputs/GraphEdgeInputHandler.js';
+import InputContext from 'util/input/InputContext.js';
+
+import GraphNode from 'graph2/element/GraphNode.js';
+
+const DEFAULT_MOVE_MODE_FIRST = true;
+
+class InputController
+{
+	constructor()
+	{
+		this._trashMode = false;
+		this._moveMode = DEFAULT_MOVE_MODE_FIRST;
+		this._nodeOnly = false;
+		this._moveModeFirst = DEFAULT_MOVE_MODE_FIRST;
+		this._handlingInput = false;
+
+		this._target = {
+			source: null,
+			type: null
+		};
+		this._activeTarget = {
+			source: null,
+			type: null,
+			input: true
+		};
+		this._immediateTarget = {
+			source: null,
+			type: null
+		};
+
+		this._inputContext = new InputContext();
+		this._graphController = null;
+		this._selectionBox = null;
+		this._labelFormatter = null;
+
+		this.onMouseOver = this.onMouseOver.bind(this);
+		this.onMouseOut = this.onMouseOut.bind(this);
+	}
+
+	setGraphController(graphController)
+	{
+		this._graphController = graphController;
+		return this;
+	}
+
+	setSelectionBox(selectionBox)
+	{
+		this._selectionBox = selectionBox;
+		return this;
+	}
+
+	setLabelFormatter(labelFormatter)
+	{
+		this._labelFormatter = labelFormatter;
+		return this;
+	}
+
+	setMoveMode(flag)
+	{
+		this._moveMode = flag;
+		return this;
+	}
+
+	setTrashMode(flag)
+	{
+		this._trashMode = flag;
+		return this;
+	}
+
+	setNodeEventsOnly(flag)
+	{
+		this._nodeOnly = flag;
+		return this;
+	}
+
+	setMoveModeFirst(flag)
+	{
+		this._moveModeFirst = flag;
+		return this;
+	}
+
+	initialize()
+	{
+		const graphController = this._graphController;
+		if (!graphController) throw new Error("Must set graph controller before init");
+		const selectionBox = this._selectionBox;
+		if (!selectionBox) throw new Error("Must set selection box before init");
+		const labelFormatter = this._labelFormatter;
+		if (!labelFormatter) throw new Error("Must set label formatter before init");
+
+		this._inputContext
+			.addInputHandler(this)
+			.addInputHandler(new GraphEdgeInputHandler(this, graphController, labelFormatter))
+			.addInputHandler(new GraphNodeInputHandler(this, graphController, selectionBox, labelFormatter));
+	}
+
+	update()
+	{
+		// There's nothing to do yet...
+	}
+
+	destroy()
+	{
+		this._inputContext.clearInputHandlers();
+	}
+
+	/** @override */
+	onPreInputEvent(pointer)
+	{
+		this._handlingInput = true;
+		this._moveMode = !pointer.getAdapter().isAltInput();
+
+		this.updateCurrentTarget(this._immediateTarget.source, this._immediateTarget.type);
+		return false;
+	}
+
+	/** @override */
+	onPostInputEvent(pointer)
+	{
+		this.updateCurrentTarget(null, null);
+		this._handlingInput = false;
+	}
+
+	onMouseOver(e)
+	{
+		const target = e.target;
+		const value = target['value'];
+		this._immediateTarget.source = value.source;
+		this._immediateTarget.type = value.type;
+		
+		// Although you can access this, it cannot be a property for "active" targets.
+		// This is because those properties are used for comparison and equality. The
+		// extra data could be used for that, but it's up to you to manage that.
+		this._immediateTarget.data = value;
+	}
+
+	onMouseOut(e)
+	{
+		this._immediateTarget.source = null;
+		this._immediateTarget.type = null;
+
+		this._immediateTarget.data = null;
+	}
+
+	bindActiveTarget(targetSource, targetType, pointerEvent = true)
+	{
+		this._activeTarget.source = targetSource;
+		this._activeTarget.type = targetType;
+		this._activeTarget.input = pointerEvent;
+	}
+
+	unbindActiveTarget()
+	{
+		this._activeTarget.source = null;
+		this._activeTarget.type = null;
+		this._activeTarget.input = false;
+	}
+
+	updateCurrentTarget(targetSource, targetType)
+	{
+		this._target.source = targetSource;
+		this._target.type = targetType;
+	}
+
+	getActiveTargetSource() { return this._activeTarget.source; }
+	getActiveTargetType() { return this._activeTarget.type; }
+	hasActiveTarget() { return this._activeTarget.type !== null; }
+
+	hasPointerEvents(targetSource)
+	{
+		if (this._activeTarget.source === targetSource) return this._activeTarget.input;
+		if (this._nodeOnly) return targetSource instanceof GraphNode;
+		return true;
+	}
+
+	getCurrentTargetSource() { return this._target.source; }
+	getCurrentTargetType() { return this._target.type; }
+	hasCurrentTarget() { return this._target.type !== null; }
+
+	getImmediateTargetSource() { return this._immediateTarget.source; }
+	getImmediateTargetType() { return this._immediateTarget.type; }
+	hasImmediateTarget() { return this._immediateTarget.type !== null; }
+
+	isTrashMode() { return this._trashMode; }
+	isMoveMode() { return this._moveModeFirst ? this._moveMode : !this._moveMode; }
+	isNodeEventsOnly() { return this._nodeOnly; }
+	isMoveModeFirst() { return this._moveModeFirst; }
+
+	isHandlingInput() { return this._handlingInput; }
+	getInputContext() { return this._inputContext; }
+}
+
+export default InputController;
