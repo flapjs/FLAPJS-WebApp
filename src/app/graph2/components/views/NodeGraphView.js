18aa7fffe70083ddaf42777cca14746f9e51bb55
Andrew <andykuo1supergreen@gmail.com>
2019-05-06 14:07:47 -0700

    Added graph2

---
+++ src/app/graph2/components/views/NodeGraphView.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+import BoxRenderer from 'graph2/renderer/BoxRenderer.js';
+
+import GraphNodeLayer from '../layers/GraphNodeLayer.js';
+import GraphEdgeLayer from '../layers/GraphEdgeLayer.js';
+import GraphHighlightLayer from '../layers/GraphHighlightLayer.js';
+
+import InputContext from 'util/input/InputContext.js';
+import SelectionBoxInputHandler from 'graph2/inputs/SelectionBoxInputHandler.js';
+import ViewportInputHandler from 'graph2/inputs/ViewportInputHandler.js';
+
+class NodeGraphView extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+
+		this._inputContext = new InputContext();
+	}
+
+	/** @override */
+	componentDidMount()
+	{
+		const viewport = this.props.viewport;
+		const inputController = this.props.inputController;
+		const graphController = this.props.graphController;
+		const selectionBox = this.props.selectionBox;
+		const inputAdapter = viewport.getInputAdapter();
+
+		const context = this._inputContext;
+		if (selectionBox)
+		{
+			context.addInputHandler(new SelectionBoxInputHandler(inputController, graphController, selectionBox));
+		}
+		if (inputController)
+		{
+			context.addInputHandler(new ViewportInputHandler(inputController));
+		}
+		inputAdapter.bindContextAsLast(context);
+	}
+
+	/** @override */
+	componentWillUnmount()
+	{
+		const viewport = this.props.viewport;
+		const inputAdapter = viewport.getInputAdapter();
+		const context = this._inputContext;
+		context.clearInputHandlers();
+		inputAdapter.unbindContext(context);
+	}
+
+	getInputContext() { return this._inputContext; }
+
+	/** @override */
+	render()
+	{
+		const inputController = this.props.inputController;
+		const graphController = this.props.graphController;
+		const selectionBox = this.props.selectionBox;
+		const onMouseOver = this.props.onMouseOver;
+		const onMouseOut = this.props.onMouseOut;
+		
+		const graph = graphController.getGraph();
+		const selectionBoundingBox = selectionBox ? selectionBox.getBoundingBox() : null;
+
+		// Order matters here, it determines the z-index!
+		return (
+			<React.Fragment>
+				<GraphNodeLayer nodes={graph.getNodes()}
+					inputController={inputController}
+					onMouseOver={onMouseOver}
+					onMouseOut={onMouseOut} />
+				<GraphEdgeLayer edges={graph.getEdges()}
+					inputController={inputController}
+					onMouseOver={onMouseOver}
+					onMouseOut={onMouseOut} />
+				{selectionBox &&
+					<React.Fragment>
+						<GraphHighlightLayer
+							nodes={selectionBox.getSelection()} />
+						<BoxRenderer visible={selectionBox.isVisible()}
+							fromX={selectionBoundingBox.fromX}
+							fromY={selectionBoundingBox.fromY}
+							toX={selectionBoundingBox.toX}
+							toY={selectionBoundingBox.toY} />
+					</React.Fragment>}
+			</React.Fragment>
+		);
+	}
+}
+
+export default NodeGraphView;
---
+++ src/app/modules/nodegraph/NodeGraphModule.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PanelContainer from 'experimental/panels/PanelContainer.js';
+
+import * as UserUtil from 'experimental/UserUtil.js';
+import { RENDER_LAYER_WORKSPACE } from 'session/manager/RenderManager.js';
+import GraphEditorView from 'graph2/components/views/GraphEditorView.js';
+
+import NodeGraph from 'graph2/NodeGraph.js';
+import GraphNode from 'graph2/element/GraphNode.js';
+import QuadraticEdge from 'graph2/element/QuadraticEdge.js';
+import NodeGraphParser from 'graph2/NodeGraphParser.js';
+import NodeGraphController from './graph/controller/NodeGraphController.js';
+
+import NodalGraphExporter from './exporter/NodalGraphExporter.js';
+import { DEFAULT_IMAGE_EXPORTERS } from './exporter/NodalGraphImageExporter.js';
+
+const MODULE_NAME = "nodegraph";
+const MODULE_VERSION = "0.0.1";
+const MODULE_LOCALIZED_NAME = "NodeGraph";
+
+class NodalGraphModule
+{
+  constructor(app)
+  {
+    this._app = app;
+
+    this._graph = new NodeGraph(GraphNode, QuadraticEdge);
+    this._graphController = new NodeGraphController(app, this._graph, new NodeGraphParser());
+
+    app.getDrawerManager()
+      .addPanelClass(props => (
+        <PanelContainer id={props.id}
+          className={props.className}
+          style={props.style}
+          title={"Your Average Graph Editor"}>
+          <p>{"Brought to you with \u2764 by the Flap.js team."}</p>
+          <p>{"<- Tap on a tab to begin!"}</p>
+        </PanelContainer>
+      ));
+
+    app.getExportManager()
+      .addExporter(new NodalGraphExporter())
+      .addExporters(DEFAULT_IMAGE_EXPORTERS);
+
+    app.getRenderManager()
+      .addRenderer(RENDER_LAYER_WORKSPACE, props => (
+        <GraphEditorView graphController={this._graphController} />
+      ));
+  }
+
+  //Override
+  initialize(app)
+  {
+  }
+
+  //Override
+  update(app)
+  {
+  }
+
+  //Override
+  destroy(app)
+  {
+  }
+
+  //Override
+  clear(app, graphOnly = false)
+  {
+    UserUtil.userClearGraph(app, graphOnly, () => app.getToolbarComponent().closeBar());
+  }
+
+  getGraphController() { return this._graphController; }
+
+  //Override
+  getModuleVersion() { return MODULE_VERSION; }
+  //Override
+  getModuleName() { return MODULE_NAME; }
+  //Override
+  getLocalizedModuleName() { return MODULE_LOCALIZED_NAME; }
+
+  getApp() { return this._app; }
+}
+
+export default NodalGraphModule;
