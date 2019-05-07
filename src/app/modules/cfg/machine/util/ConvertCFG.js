7c4fd1d4d3d6405ff9efde581e3fe2cc9b2c19f7
Seth D'Agostino <sdagosti@ucsd.edu>
2019-05-07 13:02:03 -0700

    Initial draft of convertToPDA done

---
+++ src/app/modules/cfg/machine/util/ConvertCFG.js
@@ -0,0 +1,68 @@
+import PDA, {EMPTY_SYMBOL} from 'modules/pda/machine/PDA.js';
+
+export function convertToPDA(cfg)
+{
+    //Check if CFG is valid
+    if(!cfg.validate()) throw new Error("Trying to convert an invalid cfg to a pda");
+
+    //Split rules by PIPE
+    cfg.separateRulesBySubstitutions(true);
+
+    const machine = new PDA();
+    const state1 = machine.createState("q1");
+    const state2 = machine.createState("q2");
+    const state3 = machine.createState("q3");
+    const state4 = machine.createState("q4");
+    let lastStateNum = 4;
+
+    //STEP 1: Push a dollar sign onto the stack to mark the bottom
+    machine.addTransition(state1, state2, EMPTY_SYMBOL, EMPTY_SYMBOL, "$");
+
+    //STEP 2: Push startVariable to the stack
+    machine.addTransition(state2, state3, EMPTY_SYMBOL, EMPTY_SYMBOL, cfg.getStartVariable());
+
+    //STEP 3: Qloop(state3). For each rule, replace the lhs variable with the rightmost symbol of the rule,
+    //keep pushing the rest of the symbols from right to left and then loop back to Qloop
+    for(const rule of cfg.getRules())
+    {
+        const lhs = rule.getLHS();
+        const rhs = rule.getRHS();
+
+        //***One symbol RHS doesn't require new states, just a self-loop to replace lhs symbol with rhs symbol***
+        if(rhs.length == 1)
+        {
+            machine.addTransition(state3, state3, EMPTY_SYMBOL, lhs, rhs);
+            continue;
+        }
+        else
+        {
+            let i = rhs.length - 1;
+            let newState = machine.createState(`q${++lastStateNum}`);
+            //Replace lhs variable with rightmost symbol
+            machine.addTransition(state3, newState, EMPTY_SYMBOL, lhs, rhs.charAt(i));
+            //Push symbols from right to left
+            for( --i ; i > 0; i-- )
+            {
+                let prevState = newState;
+                newState = machine.createState(`q${++lastStateNum}`);
+                machine.addTransition(prevState, newState, EMPTY_SYMBOL, EMPTY_SYMBOL, rhs.charAt(i));
+            }
+            //For the first rhs symbol, loop back to Qloop
+            machine.addTransition(newState, state3, EMPTY_SYMBOL, lhs, rhs.charAt(0));
+        }
+    }
+
+    //STEP 4: Pop terminals in a self-loop with Qloop
+    for(const terminal of cfg.getTerminals())
+    {
+        machine.addTransition(state3, state3, terminal, terminal, EMPTY_SYMBOL);
+    }
+
+    //STEP 5: Pop dollar sign to get to accept state
+    machine.addTransition(state3, state4, EMPTY_SYMBOL, "$", EMPTY_SYMBOL);
+
+    machine.setStartState(state1);
+    machine.setFinalState(state4);
+
+    return machine;
+}
