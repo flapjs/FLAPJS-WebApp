42d76e03bbc6d43f832303fde5cbd417332e6df2
Jacob Chazen <jchazen@ucsd.edu>
2019-05-21 12:18:36 -0700

    added support for jflap exporting

--- src/app/modules/fsa2/machine/util/sipserTestsDFAs.test.js
+++ src/app/modules/fsa2/machine/util/sipserTestsDFAs.test.js
@@ -2,6 +2,7 @@ import FSA, { EMPTY_SYMBOL } from '../FSA.js';
 import { solveFSA, convertToDFA } from '../FSAUtils.js';
 import FSAGraph from 'modules/fsa/graph/FSAGraph.js';
 import FSABuilder from 'modules/fsa2/machine/FSABuilder.js';
+import JFLAPGraphExporter from 'modules/fsa2/exporter/JFLAPGraphExporter'
 
 function testSolveFSA(machine, testString, expectedResult=true)
 {
@@ -9,7 +10,9 @@ function testSolveFSA(machine, testString, expectedResult=true)
     expect(solveFSA(machine, testString)).toBe(expectedResult);
   });
 }
-//Exmples
+
+
+//Examples
 describe("Example 1.7 Page 37", () => {
   const dfa = new FSA(false);
   const graph = new FSAGraph();
@@ -21,16 +24,21 @@ describe("Example 1.7 Page 37", () => {
   dfa.addTransition(q2,q2,"1");
   dfa.addTransition(q2,q1,"0");
   dfa.setFinalState(q2);
-
+  const module = {
+     getGraphController: function() {
+         return {
+             graph: GRAPH,
+             getGraph: function() {
+                 return graph;
+             }
+         }
+     }
+  }
   builder.getMachine().copy(dfa);
   builder.attemptBuildGraph(dfa, graph);
+  exportToFile('flapjs1_7',module)
 
-  test("start state is correct?", () => {
-    expect(graph.getStartNode().getNodeLabel()).toBe("q1");
-    expect(graph.getNodesByLabel("q2")[0].getNodeAccept()).toBe(true);
-    expect(dfa.validate()).toBe(true);
-    expect(dfa.isValid()).toBe(true);
-  });
+
 });
 //Exercises
 describe("Exercise 1.1", () => {
