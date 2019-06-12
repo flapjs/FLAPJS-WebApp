033a76f7d6d1309b19a44757d53b898aebf53dc0
Jacob Chazen <jchazen@ucsd.edu>
2019-06-12 16:48:40 -0700

    standardized jff names and removed windows carridge returns

--- src/app/modules/fsa2/machine/util/jff_comparer.sh
+++ src/app/modules/fsa2/machine/util/jff_comparer.sh
@@ -1,2 +1,4 @@
-example_names=(1_1_m1 1_1_m2 1_3 1_16_a 1_16_b 1_7 1_41 1_38 1_35 1_33 1_30 1_21 1_13 1_11)
-java -jar jflaplib-cli-1.3-bundle.jar equivalent ./JFLAP_JFFs/jflap_ex1_1_m1.jff ./JFLAP_JFFs/jflap_ex1_1_m1.jff
+example_names=(1_1_m1 1_1_m2 1_3 1_16_a 1_16_b 1_7 1_41 1_38 1_35 1_33 1_30 1_21 1_13 1_11);
+for machine in "${example_names[@]}";
+do java -jar jflaplib-cli-1.3-bundle.jar equivalent ./JFLAP_JFFs/jflap$machine ./FLAPJS_JFFs/flapjs$machine;
+done
