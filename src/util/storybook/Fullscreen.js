c3f0a04a47fbf85482e43e95265d472d5280f556
Andrew <andykuo1supergreen@gmail.com>
2019-09-12 10:14:03 -0700

    Added fullscreen addon for storybook

---
+++ src/util/storybook/Fullscreen.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import React from 'react';
+export const withFullscreen = story => (<>
+    <style>{`.sb-show-main { margin: 0; }`}</style>
+    <div style={{ width: '100vw', height: '100vh' }}>
+        {story()}
+    </div>
+</>);
