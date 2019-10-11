09f832d53efcf0f408d43c343c40e8bdb2d8e865
Jacob Chazen <jchazen@wireless-169-228-84-249.ucsd.edu>
2019-10-11 16:45:38 -0700

    test pda's first commit

---
+++ src/app/modules/pda/machine/util/testpda1.js
@@ -0,0 +1,45 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+import PDA, { EMPTY_SYMBOL } from '../FSA.js';
+import { solvePDA } from '../FSAUtils.js';
+
+function createTestMachine()
+{
+    const pda = new PDA(true);
+
+    // Build PDA...
+    // Example 1_11
+    const q0 = pda.createState("q0");
+    const q1 = pda.createState("q1");
+    const q2 = pda.createState("q2");
+
+
+    pda.addTransition(q0, q0, b, EMPTY_SYMBOL, EMPTY_SYMBOL);
+    pda.addTransition(q0, q1, a, EMPTY_SYMBOL, a);
+    pda.addTransition(q1, q2, EMPTY_SYMBOL, a, EMPTY_SYMBOL);
+    pda.setFinalState(q2);
+
+    return pda;
+}
+
+describe("Test case 1", () =>
+{
+    const pda = createTestMachine();
+    test("is a valid PDA", () =>
+    {
+        expect(pda).toBeDefined();
+        expect(pda.validate()).toBe(true);
+        expect(pda.isValid()).toBe(true);
+    });
+
+    test("has correct start state", () =>
+    {
+        expect(pda.getStartState().getStateLabel()).toBe("q0");
+    });
+
+    test("accepts 'bbabb'", () =>
+    {
+        let pda = createTestMachine();
+        expect(solvePDA(pda, "bbabb")).toBe(false);
+    });
+});
