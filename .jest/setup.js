d26332ecd0c3516f7b6afe9a6b1e50a5a1a25828
Andrew <andykuo1supergreen@gmail.com>
2019-11-02 09:24:38 -0700

    Fixed regenerator-runtime for jest

--- .jest/setup.js
+++ .jest/setup.js
@@ -1,5 +1,8 @@
 /* eslint-env node */
 
+// Prepare async/await transformers
+import 'regenerator-runtime';
+
 // Setup React 16 with Enzyme
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
