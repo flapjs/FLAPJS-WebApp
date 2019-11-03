79246f22529283a10635e9a4d2b760f013ff7778
Andrew <andykuo1supergreen@gmail.com>
2019-11-03 08:38:02 -0800

    Moved tab into panel

--- src/modules/base/AboutPanel.jsx
+++ src/modules/base/AboutPanel.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Pane from '@flapjs/components/drawer/pane/Pane.jsx';
 
-import AboutTab from './AboutTab.jsx';
+import IconButton from '@flapjs/components/icons/IconButton.jsx';
+import { RunningManIcon } from '@flapjs/components/icons/Icons.js';
 
 class AboutPanel extends React.Component
 {
@@ -20,6 +22,20 @@ class AboutPanel extends React.Component
         );
     }
 }
-AboutPanel.Tab = AboutTab;
+AboutPanel.Tab = Tab;
+
+function Tab(props)
+{
+    const { onClick, ...otherProps } = props;
+    return (
+        <IconButton
+            onClick={onClick}
+            iconClass={RunningManIcon}
+            {...otherProps}/>
+    );
+}
+Tab.propTypes = {
+    onClick: PropTypes.func.isRequired
+};
 
 export default AboutPanel;
